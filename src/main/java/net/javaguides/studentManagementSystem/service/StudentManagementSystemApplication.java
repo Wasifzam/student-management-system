d989b61ce0b06e90efd05e7063e86ff97b687bf0
Wasifzam <mdwasifpurnea754@gmail.com>
2024-06-23 13:55:45 +0530

    first commit

---
+++ src/main/java/net/javaguides/studentManagementSystem/service/StudentManagementSystemApplication.java
@@ -0,0 +1,37 @@
+package net.javaguides.studentManagementSystem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import net.javaguides.studentManagementSystem.repository.StudentRepository;
+
+
+@SpringBootApplication
+public class StudentManagementSystemApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+	@SuppressWarnings("unused")
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		/*
+		 * Student student1 = new Student("Wasif", "Fadatare", "wasif@gmail.com");
+		 * studentRepository.save(student1);
+		 * 
+		 * Student student2 = new Student("Sanjay", "Jadhav", "sanjay@gmail.com");
+		 * studentRepository.save(student2);
+		 * 
+		 * Student student3 = new Student("tiny", "stark", "tiny@gmail.com");
+		 * studentRepository.save(student3);
+		 */
+		
+	}
+
+}
---
+++ src/main/java/net/javaguides/studentManagementSystem/service/StudentService.java
@@ -0,0 +1,22 @@
+package net.javaguides.studentManagementSystem.service;
+
+import java.util.List;
+
+import net.javaguides.studentManagementSystem.entity.Student;
+
+public interface StudentService {
+	
+	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+	
+	Student getStudentById(Long id);
+	
+	
+	Student updateStudent(Student student);
+	
+	
+	void deleteStudentById(Long id);
+
+}
